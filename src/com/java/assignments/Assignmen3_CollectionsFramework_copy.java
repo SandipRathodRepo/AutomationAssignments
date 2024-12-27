e98d641876267ab541fdb9380ce8b7016c11b588
Sandip Rathod <sandip.rathod5592@gmail.com>
2024-12-27 18:48:01 +0530

    Assignment 3 collections Framework

--- src/com/java/assignments/Assignmen3_CollectionsFramework_copy.java
+++ src/com/java/assignments/Assignmen3_CollectionsFramework_copy.java
@@ -10,11 +10,16 @@ public class Assignmen3_CollectionsFramework_copy {
 
 	public static void main(String[] args) {
 		
-		// ********  student Details ****************************************************************
+		//********  student Details ****************************************************************
+		System.out.println("**************  Start of  Student Map  *************** ");
+		
 		Map<String, String> student1Map = new LinkedHashMap<String, String>();
 		Map<String, String> student2Map = new LinkedHashMap<String, String>();
 		Map<String, String> student3Map = new LinkedHashMap<String, String>();
 		
+		System.out.println(student1Map);
+		System.out.println(student2Map);
+		System.out.println(student3Map);
 
 		//Map To Store All information/Data of Student1
 		student1Map.put("Name", "John Doe");
@@ -27,6 +32,10 @@ public class Assignmen3_CollectionsFramework_copy {
 		student1Map.put("Email", "John@example.com");
 		student1Map.put("Contact Number", "9999997777");
 		student1Map.put("Address", "123 Elm St");
+		
+		System.out.println(student1Map);
+		System.out.println(student2Map);
+		System.out.println(student3Map);
 
 		//Map To Store All information/Data of Student2
 		student2Map.put("Name", "JaneSmith");
@@ -40,6 +49,10 @@ public class Assignmen3_CollectionsFramework_copy {
 		student2Map.put("Contact Number", "9999996666");
 		student2Map.put("Address", "456 Oak St");
 
+		System.out.println(student1Map);
+		System.out.println(student2Map);
+		System.out.println(student3Map);
+		
 		//Map To Store All information/Data of Student3
 		student3Map.put("Name", "MikeBrown");
 		student3Map.put("age", "22");
@@ -51,13 +64,24 @@ public class Assignmen3_CollectionsFramework_copy {
 		student3Map.put("Email", "mike@example.com");
 		student3Map.put("Contact Number", "9999991111");
 		student3Map.put("Address", "789 Pine St");
-
-		// ********   Employee Details ****************************************************************
+		
+		System.out.println(student1Map);
+		System.out.println(student2Map);
+		System.out.println(student3Map);
+		System.out.println("                          ");
+		System.out.println(" End of Student Map  ======================================================== ");
+		
+		//********   Employee Details ****************************************************************
+		System.out.println("**************  Start of  Employee Map  *************** ");
+		
 		Map<String, String> employee1Map = new LinkedHashMap<String, String>();
 		Map<String, String> employee2Map = new LinkedHashMap<String, String>();
 		Map<String, String> employee3Map = new LinkedHashMap<String, String>();
 
-		
+		System.out.println(employee1Map);
+		System.out.println(employee2Map);
+		System.out.println(employee3Map);
+
 		//Map To Store All information/Data of Employee1 
 		employee1Map.put("Employee Id", "E001");
 		employee1Map.put("Name", "Alice Green");
@@ -69,6 +93,10 @@ public class Assignmen3_CollectionsFramework_copy {
 		employee1Map.put("Email", "alice@example.com");
 		employee1Map.put("Contact Number", "9876544613");
 		
+		System.out.println(employee1Map);
+		System.out.println(employee2Map);
+		System.out.println(employee3Map);
+		
 		//Map To Store All information/Data of Employee2
 		employee2Map.put("Employee Id", "E002");
 		employee2Map.put("Name", "Bob Johnson");
@@ -79,6 +107,10 @@ public class Assignmen3_CollectionsFramework_copy {
 		employee2Map.put("Salary", "85000");
 		employee2Map.put("Email", "bob@example.com");
 		employee2Map.put("Contact Number", "9876544614");
+		
+		System.out.println(employee1Map);
+		System.out.println(employee2Map);
+		System.out.println(employee3Map);
 
 		//Map To Store All information/Data of Employee3
 		employee3Map.put("Employee Id", "E003");
@@ -90,12 +122,23 @@ public class Assignmen3_CollectionsFramework_copy {
 		employee3Map.put("Salary", "65000");
 		employee3Map.put("Email", "carol@example.com");
 		employee3Map.put("Contact Number", "9876544615");
+		
+		System.out.println(employee1Map);
+		System.out.println(employee2Map);
+		System.out.println(employee3Map);
+		System.out.println(" End of  Employee Map  ==================================================== ");
 
-		// ********   Product Details ****************************************************************
+		//********   Product Details ****************************************************************
+		
+		System.out.println("**************  Start of  Product Map  *************** ");
 		Map<String, String> product1Map = new LinkedHashMap<String, String>();
 		Map<String, String> product2Map = new LinkedHashMap<String, String>();
 		Map<String, String> product3Map = new LinkedHashMap<String, String>();
-
+		
+		System.out.println(product1Map);
+		System.out.println(product2Map);
+		System.out.println(product3Map);
+		
 		//Map To Store All information/Data of  Product1 Details
 		product1Map.put("ProducID", "P001");
 		product1Map.put("Name", "Laptop");
@@ -108,6 +151,10 @@ public class Assignmen3_CollectionsFramework_copy {
 		product1Map.put("Manufacturing Date", "15-03-2023");
 		product1Map.put("Expiry Date", "15-01-2025");
 
+		System.out.println(product1Map);
+		System.out.println(product2Map);
+		System.out.println(product3Map);
+		
 		//Map To Store All information/Data of Product2 Details
 		product2Map.put("ProducID", "P002");
 		product2Map.put("Name", "Desk chair");
@@ -120,6 +167,10 @@ public class Assignmen3_CollectionsFramework_copy {
 		product2Map.put("Manufacturing Date", "10-02-2023");
 		product2Map.put("Expiry Date", "NA");
 		
+		System.out.println(product1Map);
+		System.out.println(product2Map);
+		System.out.println(product3Map);
+		
 		//Map To Store All information/Data of Product3 Details
 		product3Map.put("ProducID", "P003");
 		product3Map.put("Name", "Cofee Maker");
@@ -131,23 +182,34 @@ public class Assignmen3_CollectionsFramework_copy {
 		product3Map.put("Rating", "4.2");
 		product3Map.put("Manufacturing Date", "20-03-2023");
 		product3Map.put("Expiry Date", "20-03-2024");
+		
+		System.out.println(product1Map);
+		System.out.println(product2Map);
+		System.out.println(product3Map);
+		System.out.println("  End of  product Map  ================================================= ");
 
 		// List to Store All the Maps of Student Details, employee details, product details
 		
 		List<Map<String, String>> studentList = new ArrayList<Map<String, String>>();
+		System.out.println(studentList);
 		studentList.add(student1Map);
 		studentList.add(student2Map);
 		studentList.add(student3Map);
+		System.out.println(studentList);
+		
+		System.out.println("............................ studentList  ....................... ");
 		System.out.println(studentList.get(0));
 		System.out.println(studentList.get(1));
 		System.out.println(studentList.get(2));
 		//System.out.println(studentList.get(0).get("Name"));
 		
 		List<Map<String, String>> employeeList = new ArrayList<Map<String, String>>();
+		
 		employeeList.add(employee1Map);
 		employeeList.add(employee2Map);
 		employeeList.add(employee3Map);
 		
+		System.out.println(" ............................employeeList  ....................... ");
 		System.out.println(employeeList.get(0));
 		System.out.println(employeeList.get(1));
 		System.out.println(employeeList.get(2));
@@ -157,11 +219,15 @@ public class Assignmen3_CollectionsFramework_copy {
 		productList.add(product2Map);
 		productList.add(product3Map);
 		
+		System.out.println(" .............................productList  ....................... ");
 		System.out.println(productList.get(0));
 		System.out.println(productList.get(1));
 		System.out.println(productList.get(2));
 		
 		Map<String, List<Map<String, String>>> data = new HashMap<String, List<Map<String, String>>>();
+		System.out.println("::::::::::::::::::::::: data map ::::::::::::::::::::::::::::::::" +data);
+		System.out.println(data);
+		
 		data.put("StudentData", studentList);
 		data.put("EmployeeData", employeeList);
 		data.put("ProductData", productList);
